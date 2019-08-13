4d1698faef999b7963b0f39e6d53fdeca10527b1
chenchaohan <chenchaohan@live.com>
2019-08-13 17:36:30 +0800

    router 参数 push && replace 区别

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{name:'home'}">home</router-link>
+      <router-link :to="{name:'about'}">about</router-link>
     </div>
-    <router-view/>
+    <router-view />
+    <router-view name="email" />
+    <router-view name="tel" />
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -19,6 +21,7 @@
 #nav {
   padding: 30px;
   a {
+    padding-right: 20px;
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
